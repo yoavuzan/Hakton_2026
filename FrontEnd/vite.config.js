84f96ddc75eb7343efbf43c52eadc7b1dff0970a
idogardana <idogardana21@gmail.com>
2026-04-30 03:18:36 +0300

    Update vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    allowedHosts: 'all',
+    allowedHosts: ['hakton-2026-7ptp.onrender.com'],
     host: true,
     port: 5173
   }
